10dd2b4e8da62f35e5a7e6752e33d469d0cf75b8
Guliette <you@example.com>
2025-10-05 00:37:20 +0500

    done

--- src/app/provider/index.tsx
+++ src/app/provider/index.tsx
@@ -2,7 +2,6 @@ import { StrictMode, Suspense, type ReactNode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { PrimeReactProvider } from "primereact/api";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "../../shared/style/index.css";
 
 const client = new QueryClient();
 
