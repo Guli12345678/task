6430797c7c34d9c568cd4a73aaa57ba1809f7a76
Guliette <you@example.com>
2025-10-05 14:28:59 +0500

    finished

--- src/app/provider/index.tsx
+++ src/app/provider/index.tsx
@@ -6,15 +6,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const client = new QueryClient();
 
 const AppProvider = ({ children }: { children: ReactNode }) => (
-  <StrictMode>
-    <PrimeReactProvider>
-      <BrowserRouter>
-        <QueryClientProvider client={client}>
-          <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
-        </QueryClientProvider>
-      </BrowserRouter>
-    </PrimeReactProvider>
-  </StrictMode>
+  <PrimeReactProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={client}>
+        <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+      </QueryClientProvider>
+    </BrowserRouter>
+  </PrimeReactProvider>
 );
 
 export default AppProvider;
